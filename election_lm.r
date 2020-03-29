5a073e8f89c574babe76697c0b40e792435f32df
sclee0624 <60369001+sclee0624@users.noreply.github.com>
2020-03-29 17:25:09 -0500

    Add files via upload

--- election_lm.r
+++ election_lm.r
@@ -3,7 +3,8 @@
   sang_election = read.csv(file = "election_couty_cleaned.csv")
   load(file = "full_data.Rdata")
 }
-
+library(glmnet)
+library(caret)
 
 head(full_data[,1:4])
 head(sang_election[,1:4])
@@ -19,46 +20,89 @@ m2 = (which(sang_election[,4] %in% full_data[,3]))
 data_response_match = sang_election[m2,]
 data_response_match = sang_election[-729,]
 
-
-
 data_response_match = data.frame(data_response_match[,3:4] ,data_response_match[,59:74])
+head(data_response_match)
 
-y = abs(data_response_match$Demvotes16 - data_response_match$Demvotes12)
-y = scale(y)
+# log odds 
+logOdds_dem2016 = log(data_response_match$pct_dem_16/(1-data_response_match$pct_dem_16))
+logOdds_dem2012 = log(data_response_match$pct_dem_12/(1-data_response_match$pct_dem_12))
+
+y = logOdds_dem2016 - logOdds_dem2012
+
+# y = abs(data_response_match$Demvotes16 - data_response_match$Demvotes12)
+# y = scale(y)
 
 X = data_feature_meatch[,-1:-4]
 X = apply(X, 2, scale)
 
-cv_lasso = cv.glmnet(X, y)
-lasso_coef = coef(cv_lasso, s = "lambda.min")
-
-lasso_vs = X[,-which(lasso_coef[-1,1]==0)]
-
-X_features = data_feature_meatch[,-1:-4]
-par(mfrow = c(2,2))
+# set.seed(101)
+# cv_lasso = cv.glmnet(X, y)
+# lasso_coef = coef(cv_lasso, s = "lambda.min")
+# lasso_vs = X[,-which(lasso_coef[-1,1]==0)]
 
 
-X_features = as.matrix(X_features)
-fit_lm = lm(y~X_features)
-plot(fit_lm)
+df = data.frame(y, X)
+set.seed(101)
+model <- train(
+  y ~., data = df, method = "glmnet",
+  family = "gaussian",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 10
+)
+
+# Best tuning parameter
+model$bestTune
+elastic_coef = coef(model$finalModel, model$bestTune$lambda)
+
+?glmnet
+
+# removing all variables which coefficients are zero 
+a = which(elastic_coef[-1] == 0)
+X = X[,-a]
+data = data.frame(y, X)
+head(data)
+dim(data)
 
 
-# generalized log transforamtion 
-# z <- 1
-# x <- selected.data_3D_exp[,-1]
-# glog_3Dexp <- log2((x+sqrt((x^2)+(z^2)))/2)
+# X_features = data_feature_meatch[,-1:-4]
+# par(mfrow = c(2,2))
+
+
+# data = data.frame(data_response_match[,1:2], y, lasso_vs, check.names = FALSE)
+
+
+set.seed(101)
+indexing <- createDataPartition(y = data$y, p = 0.75,list = FALSE)
+data_tr = data[indexing,]
+data_tst = data[-indexing,]
+data_train = data_tr[,-1:-2]
+data_test = data_tst[,-1:-2]
+head(data_train)
 
 
 
-df = insert(a, 4, "State")
-df = df[-65]
-colnames(full_data) = df
+knn_fit = train(y~., 
+                data = data_train, 
+                trControl = trainControl(method = 'repeatedcv', number = 10, repeats = 3), 
+                method = 'knn', 
+                tuneLength = 10
+                )
+plot(knn_fit)
+knn_pred = predict(knn_fit, newdata = data_test)
+mean((data_test$y - knn_pred)^2)
+
+
+rf_fit = train(y~., 
+               data = data_train, 
+               trControl = trainControl(method = 'repeatedcv', number = 10, repeats = 3), 
+               method = 'rf', 
+               tuneLength = 5
+               )
+rf_pred = predict(rf_fit, newdata = data_test)
+mean((data_test$y - rf_pred)^2)
 
 
 
-write.csv(lasso_coef, file = "lasso_coef.csv")
-install.packages("EnvStats")
-library(EnvStats)
 
 
 
